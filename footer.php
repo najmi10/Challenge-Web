52695395caa2bff004becca72bd4ad0be9ea77d5
najmi10 <53500211+najmi10@users.noreply.github.com>
2020-04-05 22:49:11 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<script src="http://localhost/nilai_siswa/aset/jquery.js"></script>
+    <script src="http://localhost/nilai_siswa/aset/bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="http://localhost/nilai_siswa/aset/bootstrap/css/bootstrap.min.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+
+    <title>Data Nilai Siswa</title>
+</head>
+<body>
+    
+    
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#"><i class="fas fa-book-reader text-white mr-2"></i>Data Siswa| Hai, admin</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link" href="http://localhost/nilai_siswa/index.php">Dashboard</a>
+      <a class="nav-item nav-link" href="http://localhost/nilai_siswa/siswa/index.php">Data Siswa</a>
+      <a class="nav-item nav-link" href="#">Logout</a>
+      </div>
+  </div>
+</nav>
+</body>
+</html>
